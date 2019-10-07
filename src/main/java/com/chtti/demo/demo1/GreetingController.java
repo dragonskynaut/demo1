d5599ed162fae27265943d303be8854bb21a0401
dragon <dragonskynaut@gmail.com>
2019-10-07 16:30:43 +0800

    initial D project3

--- src/main/java/com/chtti/demo/demo1/GreetingController.java
+++ src/main/java/com/chtti/demo/demo1/GreetingController.java
@@ -1,10 +1,16 @@
 package com.chtti.demo.demo1;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class GreetingController {
-    public String greeting(){
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value = "name",
+            required = false, defaultValue = "dragon") String name, Model model) {
+        model.addAttribute("name", name);
         return "page1";
     }
 
